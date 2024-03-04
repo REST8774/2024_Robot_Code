eb8791ded7aeceba24db406a5082f1178f102d16
SumnerHull <156246723+SumnerHull@users.noreply.github.com>
2024-03-03 23:07:20 -0500

    updaed code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,156 +1,247 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import frc.robot.autonomous.AutoChooser;
-import frc.robot.autonomous.AutoRunner;
-import frc.robot.autonomous.tasks.Task;
-import frc.robot.controls.controllers.DriverController;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Subsystem;
-import frc.robot.subsystems.leds.LEDs;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.net.PortForwarder;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  // Controller
-  private final DriverController m_driverController = new DriverController(0, true, true);
+    private static final int FRONT_LEFT_CHANNEL = 5;
+    private static final int REAR_LEFT_CHANNEL = 4;
+    private static final int FRONT_RIGHT_CHANNEL = 2;
+    private static final int REAR_RIGHT_CHANNEL = 3;
+    private static final int INTAKE_LEVER_CHANNEL = 7;
+    private static final int INTAKE_VACUUM_CHANNEL = 6;
+    private static final int SHOOTER_L = 8;
+    private static final int SHOOTER_R = 9;
 
+    private CANSparkMax frontLeft, frontRight, rearLeft, rearRight, intakeLever, intakeVacuum, sparkshooterL, sparkshooterR;
+    private Joystick driveJoystick;
+    private XboxController operatorController;
+    private MecanumDrive robotDrive;
 
-  // Robot subsystems
-  private List<Subsystem> m_allSubsystems = new ArrayList<>();
-  private final Intake m_intake = Intake.getInstance();
+    private double startTime;
+    private double shooterSpeed = 0.95;
+    private double spinSpeedDefault = 0.2;
+    private double spinSpeed = spinSpeedDefault;
+    private DutyCycleEncoder pivotEncoder;
+    private DigitalInput noteSensor;
 
-  public final LEDs m_leds = LEDs.getInstance();
+    private double pivotangle;
+    private double leverAngle = 0.48;
 
-  // Auto stuff
-  private Task m_currentTask;
-  private AutoRunner m_autoRunner = AutoRunner.getInstance();
-  private AutoChooser m_autoChooser = new AutoChooser();
+    @Override
+    public void robotInit() {
+        noteSensor = new DigitalInput(8);
+        pivotEncoder = new DutyCycleEncoder(9);
+        frontLeft = new CANSparkMax(FRONT_LEFT_CHANNEL, MotorType.kBrushless);
+        rearLeft = new CANSparkMax(REAR_LEFT_CHANNEL, MotorType.kBrushless);
+        frontRight = new CANSparkMax(FRONT_RIGHT_CHANNEL, MotorType.kBrushless);
+        rearRight = new CANSparkMax(REAR_RIGHT_CHANNEL, MotorType.kBrushless);
+        intakeLever = new CANSparkMax(INTAKE_LEVER_CHANNEL, MotorType.kBrushless);
+        intakeVacuum = new CANSparkMax(INTAKE_VACUUM_CHANNEL, MotorType.kBrushless);
+        sparkshooterL = new CANSparkMax(SHOOTER_L, MotorType.kBrushless);
+        sparkshooterR = new CANSparkMax(SHOOTER_R, MotorType.kBrushless);
 
-  /**
-   * This function is run when the robot is first started up.
-   */
-  @Override
-  public void robotInit() {
-    m_allSubsystems.add(m_intake);
-    m_allSubsystems.add(m_leds);
-  }
+        driveJoystick = new Joystick(0);
+        operatorController = new XboxController(1);
 
-  @Override
-  public void robotPeriodic() {
-    m_allSubsystems.forEach(subsystem -> subsystem.periodic());
-    m_allSubsystems.forEach(subsystem -> subsystem.writePeriodicOutputs());
-    m_allSubsystems.forEach(subsystem -> subsystem.outputTelemetry());
-    m_allSubsystems.forEach(subsystem -> subsystem.writeToLog());
-  }
+        frontRight.setInverted(true);
+        rearRight.setInverted(true);
+        sparkshooterL.setInverted(true);
 
-  @Override
-  public void autonomousInit() {
-    m_autoRunner.setAutoMode(m_autoChooser.getSelectedAuto());
-    m_currentTask = m_autoRunner.getNextTask();
+        robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-    // Start the first task
-    if (m_currentTask != null) {
-      m_currentTask.start();
-    }
-  }
+        //LimeLight Code
 
-  @Override
-  public void autonomousPeriodic() {
-    // If there is a current task, run it
-    if (m_currentTask != null) {
-      // Run the current task
-      m_currentTask.update();
-      m_currentTask.updateSim();
-
-      // If the current task is finished, get the next task
-      if (m_currentTask.isFinished()) {
-        m_currentTask.done();
-        m_currentTask = m_autoRunner.getNextTask();
-
-        // Start the next task
-        if (m_currentTask != null) {
-          m_currentTask.start();
+        for (int port = 5800; port <= 5807; port++) {
+            PortForwarder.add(port, "limelight.local", port);
         }
-      }
+
+
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
+
+        //post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
     }
-  }
 
-  @Override
-  public void teleopInit() {
-  }
-
-  double speed = 0;
-
-  @Override
-  public void teleopPeriodic() {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    
-
-    // Intake
-    if (m_driverController.getWantsFullIntake()) {
-      m_intake.goToGround();
-    } else if (m_driverController.getWantsIntake()) {
-      if (m_intake.getIntakeHasNote()) {
-        m_intake.pulse();
-      } else {
-        m_intake.intake();
-      }
-    } else if (m_driverController.getWantsEject()) {
-      m_intake.eject();
-    } else if (m_driverController.getWantsSource()) {
-      m_intake.goToSource();
-    } else if (m_driverController.getWantsStow()) {
-      m_intake.goToStow();
-    } else {
-      m_intake.stopIntake();
-    
+    @Override
+    public void autonomousInit() {
+        startTime = Timer.getFPGATimestamp();
     }
-  }
 
-  @Override
-  public void disabledInit() {
-    m_leds.rainbow();
-    speed = 0;
-    m_allSubsystems.forEach(subsystem -> subsystem.stop());
-  }
+    @Override
+    public void autonomousPeriodic() {
+        double time = Timer.getFPGATimestamp() - startTime;
+        double pivotSpeed;
+        pivotangle = pivotEncoder.getAbsolutePosition();
+        if (time <= 1) {
+            sparkshooterL.set(shooterSpeed);
+            sparkshooterR.set(shooterSpeed);
+        } else if (time <= 3) {
+            intakeVacuum.set(-1);
+        } else if (time <= 6) {
+            intakeVacuum.set(0);
 
-  @Override
-  public void disabledPeriodic() {
-  }
+            pivotSpeed = (0.22 - pivotangle) / 0.3;
+            intakeLever.set(pivotSpeed);
+        } else if (time <= 8) {
+            frontLeft.set(0.2);
+            frontRight.set(0.2);
+            rearLeft.set(0.2);
+            rearRight.set(0.2);
+        }else if (time <= 9) {
+            intakeVacuum.set(.6);
+            frontLeft.set(0.1);
+            frontRight.set(0.1);
+            rearLeft.set(0.1);
+            rearRight.set(0.1);
+        } else if (time <= 12) {
+            frontLeft.set(0);
+            frontRight.set(0);
+            rearLeft.set(0);
+            rearRight.set(0);
+            pivotSpeed = (0.77 - pivotangle) / 0.3;
+            intakeLever.set(pivotSpeed);
+        } else if (time <= 14) {
+            frontLeft.set(-0.3);
+            frontRight.set(-0.3);
+            rearLeft.set(-0.3);
+            rearRight.set(-0.3);
+        } else if (time <= 15) {
+            frontLeft.set(0);
+            frontRight.set(0);
+            rearLeft.set(0);
+            rearRight.set(0);
+            intakeVacuum.set(-1);
+        } else {
+            frontLeft.set(0);
+            frontRight.set(0);
+            rearLeft.set(0);
+            rearRight.set(0);
+            sparkshooterL.set(0);
+            sparkshooterR.set(0);
+            intakeVacuum.set(0);
+        }
+    }
 
-  @Override
-  public void disabledExit() {
-  }
+    @Override
+    public void teleopInit() {
+        intakeVacuum.set(0);
+    }
 
-  @Override
-  public void testInit() {
-  }
+    @Override
+    public void teleopPeriodic() {
+        if (driveJoystick.getTrigger())
+            spinSpeed = 1.0;
+        else
+            spinSpeed = spinSpeedDefault;
 
-  @Override
-  public void testPeriodic() {
-  }
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
 
-  @Override
-  public void simulationInit() {
-  }
+        //read values periodically
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0);
+        double area = ta.getDouble(0.0);
 
-  @Override
-  public void simulationPeriodic() {
-  }
+        //post to smart dashboard periodically
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+
+        double adjustCam;
+        double speed = -driveJoystick.getY(); // Remove the negative sign here
+        double turn = driveJoystick.getZ() * spinSpeed;
+        double strafe = driveJoystick.getX();
+        double pivotangle = pivotEncoder.getAbsolutePosition();
+        double leverAngle = 0.48;
+
+        // ShuffleBoard
+        SmartDashboard.putNumber("Bridge Angle", pivotangle);
+        SmartDashboard.putNumber("Have Note", noteSensor.get() ? 0 : 1);
+        intakeLever.getAbsoluteEncoder();
+
+        if (Math.abs(turn) < 0.1) {
+            turn = 0;
+        }
+
+        // Other teleop logic goes here
+        if (operatorController.getAButton()) {
+            intakeVacuum.set(1);
+        } else if (operatorController.getXButton()) {
+            intakeVacuum.set(-0.6);
+        } else {
+            intakeVacuum.set(0);
+        }
+
+        if (driveJoystick.getRawButton(2)) {
+            adjustCam = (((x + 10) / 10)) * 0.1;
+
+            frontLeft.set(adjustCam);
+            frontRight.set(-adjustCam); // Invert the sign for moving backwards
+            rearLeft.set(adjustCam);
+            rearRight.set(-adjustCam);
+        } else {
+            robotDrive.driveCartesian(speed, strafe, turn);
+            frontLeft.set((speed + turn + strafe) * 0.5);
+            frontRight.set((speed - turn - strafe) * 0.5); // Invert the sign for moving backwards
+            rearLeft.set((speed + turn - strafe) * 0.5);
+            rearRight.set((speed - turn + strafe) * 0.5); // Invert the sign for moving backwards
+        }
+
+        if (operatorController.getRightTriggerAxis() > 0.5) {
+            sparkshooterL.set(1.0);
+            sparkshooterR.set(1.0);
+        } else if (operatorController.getRightBumper()) {
+            sparkshooterL.set(shooterSpeed);
+            sparkshooterR.set(shooterSpeed);
+        } else if (operatorController.getLeftBumper()) {
+            sparkshooterL.set(0);
+            sparkshooterR.set(0);
+        }
+
+        double leverSpeed = operatorController.getLeftY();
+        if (operatorController.getLeftStickButton()) {
+            if (pivotangle < leverAngle)
+                leverSpeed = (leverAngle - pivotangle) / 0.3;
+            else if (pivotangle > leverAngle)
+                leverSpeed = (leverAngle - pivotangle) / 0.3;
+        } else if (Math.abs(leverSpeed) > 0.1) {
+            leverSpeed = leverSpeed * 0.75;
+        } else {
+            leverSpeed = 0.0;
+        }
+
+        if ((pivotangle > 0.77 && leverSpeed > 0) || (pivotangle < 0.22 && leverSpeed < 0)) {
+            leverSpeed = 0.0;
+        }
+        intakeLever.set(leverSpeed);
+    }
 }
--- src/main/java/frc/robot/subsystems/Intake_PneumaticsSubsystem.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Compressor;
-
-public class Intake_PneumaticsSubsystem {
-
-}
